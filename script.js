15437e3e2b6f521f74d7c90b35a3ba90c701bdd2
37397398371280 <99205811+37397398371280@users.noreply.github.com>
2024-03-30 12:07:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ const saveImage = () => {
 
     ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
     ctx.translate(canvas.width / 2, canvas.height / 2);
-    if(rotate !== 0) {
+    if (rotate !== 0) {
         ctx.rotate(rotate * Math.PI / 180);
     }
     ctx.scale(flipHorizontal, flipVertical);
