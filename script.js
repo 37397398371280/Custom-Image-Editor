1a74e744c4822df6a37519be1c88e8fda284ab0d
37397398371280 <99205811+37397398371280@users.noreply.github.com>
2024-03-30 11:58:44 -0500

    Update script

--- script.js
+++ script.js
@@ -92,11 +92,6 @@ const resetFilter = () => {
 }
 
 const saveImage = () => {
-    // Assuming previewImg is defined somewhere in your code
-    const previewImg = document.getElementById("previewImg");
-
-    // Assuming brightness, saturation, inversion, grayscale, rotate,
-    // flipHorizontal, and flipVertical are defined elsewhere in your code
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     
@@ -106,7 +101,7 @@ const saveImage = () => {
     ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
     ctx.translate(canvas.width / 2, canvas.height / 2);
     
-    if(rotate !== 0) {
+    if (rotate !== 0) {
         ctx.rotate(rotate * Math.PI / 180);
     }
     
