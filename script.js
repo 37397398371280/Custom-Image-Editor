d04bfb7c1d7cf923ac726a27b07bdfbd86cdc28c
37397398371280 <99205811+37397398371280@users.noreply.github.com>
2024-03-30 12:02:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -94,20 +94,17 @@ const resetFilter = () => {
 const saveImage = () => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-    
     canvas.width = previewImg.naturalWidth;
     canvas.height = previewImg.naturalHeight;
-    
+
     ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
     ctx.translate(canvas.width / 2, canvas.height / 2);
-    
-    if (rotate !== 0) {
+    if(rotate !== 0) {
         ctx.rotate(rotate * Math.PI / 180);
     }
-    
-    ctx.scale(flipHorizontal ? -1 : 1, flipVertical ? -1 : 1);
+    ctx.scale(flipHorizontal, flipVertical);
     ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    
+
     const link = document.createElement("a");
     link.download = "image.jpg";
     link.href = canvas.toDataURL();
